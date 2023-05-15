5efe3b4fc6e563140a24781d4c2535301a13026e
StarL0st <65464466+StarL0st@users.noreply.github.com>
2023-05-15 13:50:33 -0700

    Remove NoTreePunching Recipes for vanilla planks, modded still missing

---
+++ overrides/groovy/postInit/recipes/stoneage.groovy
@@ -0,0 +1,15 @@
+
+//removals
+
+//vanilla planks - NoTreePunching
+crafting.remove("notreepunching:saw_planks_1")
+crafting.remove("notreepunching:saw_planks_2")
+crafting.remove("notreepunching:saw_planks_3")
+crafting.remove("notreepunching:saw_planks_4")
+crafting.remove("notreepunching:saw_planks_5")
+crafting.remove("notreepunching:saw_planks_6")
+
+
+
+//GregTech
+
