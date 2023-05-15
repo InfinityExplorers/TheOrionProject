deb4afd9972f55edbf52168881bc31df554e6bfa
StarL0st <65464466+StarL0st@users.noreply.github.com>
2023-05-15 12:51:58 -0700

    Add NoTreePunching, Thaumcraft 6, Thaumic Augmentation, Custom Loading Screen and Custom Main Menu + some config changes and recipe removals for earlygame steel

---
+++ overrides/groovy/postInit/recipes/removed.groovy
@@ -0,0 +1,45 @@
+// Block of Coke * 1
+mods.gregtech.coke_oven.removeByInput(1, [item('minecraft:coal_block')], null)
+
+// Steel Ingot * 1
+mods.gregtech.primitive_blast_furnace.removeByInput(1, [metaitem('ingotWroughtIron'), metaitem('dustCoke')], null)
+
+// Steel Ingot * 1
+mods.gregtech.primitive_blast_furnace.removeByInput(1, [metaitem('ingotWroughtIron'), metaitem('dustCoal') * 2], null)
+
+// Steel Ingot * 1
+mods.gregtech.primitive_blast_furnace.removeByInput(1, [metaitem('ingotWroughtIron'), metaitem('dustCharcoal') * 2], null)
+
+// Steel Ingot * 1
+mods.gregtech.primitive_blast_furnace.removeByInput(1, [metaitem('ingotWroughtIron'), item('minecraft:coal') * 2], null)
+
+// Steel Ingot * 1
+mods.gregtech.primitive_blast_furnace.removeByInput(1, [metaitem('ingotWroughtIron'), item('minecraft:coal', 1) * 2], null)
+
+// Steel Ingot * 1
+mods.gregtech.primitive_blast_furnace.removeByInput(1, [item('minecraft:iron_ingot'), metaitem('dustCoke')], null)
+
+// Steel Ingot * 1
+mods.gregtech.primitive_blast_furnace.removeByInput(1, [item('minecraft:iron_ingot'), metaitem('dustCoal') * 2], null)
+
+// Steel Ingot * 1
+mods.gregtech.primitive_blast_furnace.removeByInput(1, [item('minecraft:iron_ingot'), metaitem('dustCharcoal') * 2], null)
+
+// Steel Ingot * 1
+mods.gregtech.primitive_blast_furnace.removeByInput(1, [item('minecraft:iron_ingot'), item('minecraft:coal') * 2], null)
+
+// Steel Ingot * 1
+mods.gregtech.primitive_blast_furnace.removeByInput(1, [item('minecraft:iron_ingot'), item('minecraft:coal', 1) * 2], null)
+
+
+// Steel Ingot * 1
+mods.gregtech.electric_blast_furnace.removeByInput(120, [metaitem('ingotWroughtIron')], [fluid('oxygen') * 200])
+
+// Steel Ingot * 1
+mods.gregtech.electric_blast_furnace.removeByInput(120, [item('minecraft:iron_ingot')], [fluid('oxygen') * 200])
+
+// Steel Ingot * 1
+mods.gregtech.electric_blast_furnace.removeByInput(120, [metaitem('dustSteel')], null)
+
+// Steel Ingot * 1
+mods.gregtech.electric_blast_furnace.removeByInput(120, [metaitem('dustSteel')], null)
